391621d133f38fcdb5e7b6a4656f25f90c0d9d1e
MB <mb@m1k.pw>
2019-01-22 16:53:17 +0200

    Updated with better base Model

--- sqlalchemy/blaster-sql-base.py
+++ sqlalchemy/blaster-sql-base.py
@@ -3,6 +3,7 @@ import logging
 
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
+from sqlalchemy.ext.declarative import declared_attr
 
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -17,14 +18,15 @@ def not_read_only(func):
     return wrapped
 
 
-def make_sqlalchemy_engine(conn_params:dict):
+def make_sqlalchemy_engine(conn_params: dict):
     """
     Connection params with following keys: db_host, db_port, db_user, db_pwd, db_name.
 
     :param conn_params:
     :return:
     """
-    connection_string = f"postgresql+psycopg2://{conn_params['db_host']}:{conn_params['db_pwd']}@{conn_params['db_host']}:{conn_params['db_port']}/{conn_params['db_name']}"
+    connection_string = f"postgresql+psycopg2://{conn_params['db_host']}:" \
+        f"{conn_params['db_pwd']}@{conn_params['db_host']}:{conn_params['db_port']}/{conn_params['db_name']}"
     engine = create_engine(
         connection_string,
         pool_size=30,
@@ -33,8 +35,10 @@ def make_sqlalchemy_engine(conn_params:dict):
     del connection_string
     return engine
 
+
 db = make_sqlalchemy_engine(YOUR_CONFIG_DICT)
 Session = scoped_session(sessionmaker(db))
+
 
 def create_session(func):
     @functools.wraps(func)
@@ -58,7 +62,13 @@ class ModelBase:
     Class name must reflect the corresponding table name and end with 'Model'. For example
     if you table is called 'user', class must be named UserModel.
     """
-    __tablename__: str
+
+    @declared_attr
+    def __tablename__(cls):
+        if cls.__name__.find('Model') < 1:
+            raise NameError(f"Illegal class name {cls.__name__}, must be {cls.__name__}Model")
+        return cls.__name__[:cls.__name__.find('Model')].lower()
+
     __table_args__: dict
     __readonly__: bool = False
 
@@ -85,19 +95,32 @@ class ModelBase:
         )
         return query.all()
 
+    # New save_to_db
     @not_read_only
     def save_to_db(self):
         session = Session()
-        session.add(self)
+        # Check if a session for object is already open an utilize if it is.
+        existing_session = session.object_session(self)
         try:
+            logging.debug(f'Looking for existing session for {self}')
+            existing_session.add(self)
+            existing_session.commit()
+            logging.info(f'Saved {self} using existing object session')
+        except AttributeError:
+            logging.info(f'Existing session not found. Saving {self} using new session')
+            session.add(self)
             session.commit()
         except Exception as e:
-            logging.critical(f'SAVE TO DB FAILED: {e}')
+            logging.critical(f'Save to DB failed: {e}')
             session.rollback()
+            raise
 
     @not_read_only
     @create_session
     def delete_from_db(self, session=None):
+        existing_session = session.object_session(self)
+        if existing_session:
+            session = existing_session
         session.delete(self)
         session.commit()
 
