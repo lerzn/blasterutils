217cb455260741704f8f5d2b75726d3ef2315dbb
MB <mb@m1k.pw>
2019-02-05 12:48:03 +0200

    Refactored to be able to handle lists of commands, and parse commands and args.

--- telegramtools/command_dispatcher.py
+++ telegramtools/command_dispatcher.py
@@ -4,6 +4,17 @@ import logging
 
 
 def command_dispatch(func):
+    """
+    Decorator to register commands. Similar to functools.singledispatch.
+
+    If command contains '_' it will split it into command (before '_') and kwargs['target'] (after '_').
+    To be used as prefix for generated commands e.g. /reg_ads324asdf3242
+
+    Will put all args (/command arg1 arg2) into kwargs['args']
+
+    :param func:
+    :return:
+    """
     registry = {}
 
     def unregistered_command(bot, update, *args, **kwargs):
@@ -12,13 +23,23 @@ def command_dispatch(func):
     def dispatch(command):
         nonlocal registry
 
+        real_command = command.split('_')[0]
+
         try:
-            impl = registry[command]
+            impl = registry[real_command]
         except KeyError:
             logging.warning(f"Unregistered command: {command}")
             return unregistered_command
 
         return impl
+
+    def save_to_registry(command, func):
+        real_command = command.split('_')[0]
+
+        if real_command in registry.keys():
+            raise KeyError(f'Duplicate command {command} in {func.__module__}.{func.__name__}. '
+                           f'Command already registered in {registry[command].__module__}.{registry[command].__name__}')
+        registry[real_command] = func
 
     def register(command, func=None):
         nonlocal registry
@@ -26,7 +47,13 @@ def command_dispatch(func):
         if func is None:
             return lambda c: register(command, c)
 
-        registry[command] = func
+        if isinstance(command, list):
+            for subcommand in command:
+                save_to_registry(subcommand, func)
+        elif isinstance(command, str):
+            save_to_registry(command, func)
+        else:
+            raise ValueError(f"command must be str, or list of str, not {type(command)}")
 
         return func
 
@@ -37,6 +64,9 @@ def command_dispatch(func):
         else:
             command = text.split()[0]
             kwargs['args'] = text.split()[1:]
+
+        if '_' in command:
+            kwargs['target'] = text.split('_')[1]
 
         logging.info(f'{command} from {update.effective_user.full_name} ({update.effective_user.id})')
 
