5056fcbbb866d0776716f05ee37936c686b39a79
darynamal <dm@aisc.io>
2019-02-20 09:46:45 +0200

    Refactor custom_filters.py not to raise an error if BaseFilter is not installed

--- telegramtools/custom_filters.py
+++ telegramtools/custom_filters.py
@@ -2,15 +2,16 @@ import re
 
 try:
     from telegram.ext import BaseFilter
+
+
+    class RegexAnywhereFilter(BaseFilter):
+        pattern = None
+
+        def __init__(self, pattern):
+            self.pattern = pattern
+
+        def filter(self, message):
+            return bool(re.findall(self.pattern, message.text))
 except ModuleNotFoundError:
     pass
 
-
-class RegexAnywhereFilter(BaseFilter):
-    pattern = None
-
-    def __init__(self, pattern):
-        self.pattern = pattern
-
-    def filter(self, message):
-        return bool(re.findall(self.pattern, message.text))
